aa46670edfd252911633ddf036acf42f715c1632
Joe Arcelo <josepharcelo@gmail.com>
2016-08-16 14:45:59 -0500

    Add CatBreed and DogBreed models

---
+++ pet-rescue/Models/CatBreed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pet_rescue.Models
+{
+    public class CatBreed
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        // A cat breed will map to a collections of cat instance
+        public ICollection<Cat> Cats { get; set; }
+    }
+}
---
+++ pet-rescue/Models/DogBreed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pet_rescue.Models
+{
+    public class DogBreed
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        // A dog breed will map to a collection of dog instance
+        public ICollection<Dog> Dogs { get; set; }
+    }
+}
